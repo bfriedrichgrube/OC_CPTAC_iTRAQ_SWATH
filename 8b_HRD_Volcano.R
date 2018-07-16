c83ad817234397de5bda3ab06722b4075b472306
bfriedrichgrube <38727519+bfriedrichgrube@users.noreply.github.com>
2018-07-16 13:21:15 +0000

    Update 8b_HRD_Volcano.R

--- 8b_HRD_Volcano.R
+++ 8b_HRD_Volcano.R
@@ -14,7 +14,7 @@ output <- read.table('analysis_output.txt', sep='\t', dec='.', header=T, as.is=T
 
 ## Step 3: Plot differentially expressed proteins in Volcano plot ##
 
-pdf('Volcano_HRD_20180315.pdf', width=10, height=10)
+pdf('Volcano_HRD.pdf', width=10, height=10)
 with(output, plot(log2FC, -log10(FDR), pch=20, cex=2, cex.lab=0.1, yaxt='n', cex.axis=2.2, xlab='', ylab='', xlim=c(-1, 1)))
 with(subset(output, FDR<0.05 & log2FC>log2(1.3)), points(log2FC, -log10(FDR), pch=20, cex=2.2, col='red'))
 with(subset(output, FDR<0.05 & log2FC<(-log2(1.3))), points(log2FC, -log10(FDR), pch=20, cex=2.2, col='dodgerblue'))
