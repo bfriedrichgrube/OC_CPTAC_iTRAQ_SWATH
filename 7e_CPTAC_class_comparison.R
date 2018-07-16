dbb1b7025abd8f35ae5d663cd6632be5ec3833c0
bfriedrichgrube <38727519+bfriedrichgrube@users.noreply.github.com>
2018-07-16 13:08:13 +0000

    Update 7e_CPTAC_class_comparison.R

--- 7e_CPTAC_class_comparison.R
+++ 7e_CPTAC_class_comparison.R
@@ -1,11 +1,19 @@
-## Make group comparisons based on original  CPTAC classification ##
+## Characterize Mesenchymal cluster (Figure 5 )
+## Make group comparisons based on original  CPTAC classification (Figure 6 and S4)
 ## Mesechymal to others should show significantly higher overlap in proteins regulated than other groups## 
+
 ## Step 1: Prepare R session 
 
+setwd('')
+
 library(VennDiagram)
-setwd('Documents/Biomarker_Cancer/Ovarian_JohnHopkins/2017_10_Paper/CPTAC_Classification/')
-load('CPTAC_DDA_WGCNA_20171101.Rdata') ## load protein modules from WGCNA of DDA -> clustEG red corresponds to ECM organization
-load('../SWATH_iTRAQ_correlation.Rdata') ## load corrlations vector b corresponds to the ordered correlations of SWATH to iTRAQ ratios
+
+## Step 2: Load data
+
+load('CPTAC_DDA_WGCNA.Rdata') ## load protein modules from WGCNA of DDA -> clustEG red corresponds to ECM organization
+load('SWATH_iTRAQ_correlation.Rdata') ## load corrlations vector b corresponds to the ordered correlations of SWATH to iTRAQ ratios
+
+load('CPTAC_DDA_WGCNA.Rdata')
 
 #extract mesenchymal proteins
 
@@ -21,7 +29,7 @@ tan      29    -none- character
 
 DDA_mes <- clustEG$red
 
-load('CPTAC_SWATH_WGCNA_20171101.Rdata') ## load protein modules from WGCNA of SWATH -> clustEG black corresponds to ECM organization
+load('CPTAC_SWATH_WGCNA.Rdata') ## load protein modules from WGCNA of SWATH -> clustEG black corresponds to ECM organization
 
 #extract mesenchymal proteins
 
@@ -36,7 +44,7 @@ turquoise 1063   -none- character
 SWATH_mes <- clustEG$black
 
 
-## Step 2: Check common proteins in clusters
+## Step 3: Check common proteins in clusters
 
 a <- intersect(names(DDA_mes), names(SWATH_mes))
 
@@ -46,7 +54,7 @@ length(a)
 ## 84 proteins are in common in both ECM organization modules
 
 
-## plot overlap
+## plot overlap (Figure 5A)
 pdf('venn_mesenchymal.pdf', width=10, height=10)
 draw.pairwise.venn(283, 86, 84, category=c('DDA', 'SWATH'), lty=rep('blank', 2), fill=c('lightblue', 'green'), cat.pos=c(-25,100), cex=3, cat.cex=3,margin=0.05)
 dev.off()
@@ -55,26 +63,15 @@ dev.off()
 
 #shorten names of vector b (correlations of all proteins in common)
 x <- names(b)
-head(x)
-[1] "OSBPL8.rho"   "HERC2.rho"    "RABGGTA.rho"  "MDN1.rho"     "HLA-DRB1.rho"
-[6] "SPECC1L.rho" 
 
 y <- unlist(strsplit(x, '.rho', fixed=T))
-head(y)
-[1] "OSBPL8"   "HERC2"    "RABGGTA"  "MDN1"     "HLA-DRB1" "SPECC1L" 
-
 names(b)<- y
 head(b)
-      OSBPL8        HERC2      RABGGTA         MDN1     HLA-DRB1      SPECC1L 
--0.036627422 -0.030389228 -0.029236041 -0.019801871 -0.019648113  0.000582085 
 
 #extract just common proteins from correlations
 c <- b[a]
 length(c)
 [1] 84
-head(c)
-     FHL2     PLOD1     THBS1     PTGIS    EFEMP1    PDLIM7 
-0.7924812 0.7427075 0.7618064 0.8191473 0.8448908 0.7969512 
 
 #order
 d<- c[order(c)]
@@ -84,8 +81,8 @@ median(d)
 mean(d)
 [1] 0.7826912
 
-#plot corrleation
-pdf('Correlation_Mesenchymal_20180227.pdf', width=10, height=10)
+#plot corrleation (Figure 5B)
+pdf('Correlation_Mesenchymal.pdf', width=10, height=10)
 par(cex.axis=2.5, las=1)
 plot(d, xlab='', ylab='', xaxt='n')
 points(x=42.5, y=median(d), col='red', pch=16, cex=2)
@@ -96,43 +93,18 @@ dev.off()
 
 ## Step 4: Save relevant information
 
-save(a, d, file='CPTAC_common_proteins_mesenchymal_20180312.Rdata')
+save(a, d, file='CPTAC_common_proteins_mesenchymal.Rdata')
+q()
 
-
-## Step 1: Prepare R session 
+## Step 5: Load data for Class comparison
 
 #common proteins from mesenchymal class will be loaded ->  a and d
-load('/home/betty/Documents/Biomarker_Cancer/Ovarian_JohnHopkins/2017_10_Paper/CPTAC_common_proteins_mesenchymal_20180312.Rdata')
+load('CPTAC_common_proteins_mesenchymal.Rdata')
 
-#load DDA data -> DDA
-load('/home/betty/Documents/Biomarker_Cancer/Ovarian_JohnHopkins/2017_10_Paper/CPTAC_Data/CPTAC_processed_data/CPTAC_DDA_protein_level_CDAP.Rdata')
+load('CPTAC_DDA_overlap.Rdata')
+load('CPTAC_SWATH_overlap.Rdata')
 
-#load SWATH data -> SWATH_imputed
-load('/home/betty/Documents/Biomarker_Cancer/Ovarian_JohnHopkins/2017_10_Paper/CPTAC_Classification/SWATH_classification_workflow_20171101.Rdata')
-
-
-## Step 2: Prepare data
-
-dim(SWATH_imputed)
-[1] 1659  103
-
-dim(DDA)
-[1] 8600  103
-missing <- NULL
-for(i in 1:nrow(DDA)){tmp <-length(which(is.na(DDA[i,])))
-missing <- c(missing, tmp)
-}
-
-DDA_missing0 <- DDA[(missing==0),] 
-dim(DDA_missing0)
-[1] 4366  103
-
-gene.ids <- intersect(rownames(SWATH_imputed), rownames(DDA_missing0))
-
-SWATH_overlap <- SWATH_imputed[gene.ids, ]
-DDA_overlap <- as.matrix(DDA_missing0[gene.ids,])
-
-## Step 3: Make group comparison for Mesenchymal group and check overlap
+## Step 6: Make group comparison for Mesenchymal group and check overlap
 
 z <- c(80, 23, 27, 92, 97, 51, 60, 8:9, 71:72, 11, 15, 18, 86, 28, 30, 32:33, 37, 40, 93, 42, 99)
 length(z)
@@ -206,9 +178,9 @@ length(intersect(ax, az))
 length(d)
 [1] 84
 
-#plot
+#plot (Figure 6 A-B)
 
-pdf('Mesenchymal_SWATH_Volcano_20180314.pdf', width=10, height=10)
+pdf('Mesenchymal_SWATH_Volcano.pdf', width=10, height=10)
 with(Mes_SWATH_output, plot(log2FC, -log10(padjust), pch=20, cex=2, cex.lab=0.1, yaxt='n', cex.axis=2.2, xlab='', ylab='', xlim=c(-2, 2), ylim=c(0,22)))
 with(subset(Mes_SWATH_output, padjust<0.05 & log2FC>log2(1.3)), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='red'))
 with(subset(Mes_SWATH_output, padjust<0.05 & log2FC<(-log2(1.3))), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='dodgerblue'))
@@ -218,7 +190,7 @@ abline(h=-log10(0.05), col="darkgray", lty=2)
 dev.off()
 
 
-pdf('Mesenchymal_DDA_Volcano_20180314.pdf', width=10, height=10)
+pdf('Mesenchymal_DDA_Volcano.pdf', width=10, height=10)
 with(Mes_DDA_output, plot(log2FC, -log10(padjust), pch=20, cex=2, cex.lab=0.1, yaxt='n', cex.axis=2.2, xlab='', ylab='', xlim=c(-2, 2), ylim=c(0,22)))
 with(subset(Mes_DDA_output, padjust<0.05 & log2FC>log2(1.3)), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='red'))
 with(subset(Mes_DDA_output, padjust<0.05 & log2FC<(-log2(1.3))), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='dodgerblue'))
@@ -228,8 +200,9 @@ abline(h=-log10(0.05), col="darkgray", lty=2)
 dev.off()
 
 
+# plot Figure 6C
 
-pdf('venn_mesenchymal_Volcano_20180312.pdf', width=10, height=10)
+pdf('venn_mesenchymal_Volcano.pdf', width=10, height=10)
  draw.triple.venn(84, 80, 113, 66, 73, 69, 62, lty=rep('blank', 3), fill=c('lightpink', 'lightblue', 'green'), cex=3)
 dev.off()
 
@@ -240,7 +213,9 @@ head(common)
 write.table(common, 'common_proteins_20180313.txt' , quote=F, row.names=F)
 
 
-## Step 4: Make group comparison for Proliferative group and check overlap
+## Step 7: Make comparisons for other classes (Figure S4)
+
+#Make group comparison for Proliferative group and check overlap
 
 
 z <- c(74, 6, 68:69, 76:77, 17, 20, 29, 31, 35, 95, 50, 52, 54, 56)
@@ -300,7 +275,7 @@ length(intersect(downreg$Protein, downreg_DDA$Protein))
 
 #plot
 
-pdf('Proliferative_SWATH_Volcano_20180314.pdf', width=10, height=10)
+pdf('Proliferative_SWATH_Volcano.pdf', width=10, height=10)
 with(Pro_SWATH_output, plot(log2FC, -log10(padjust), pch=20, cex=2, cex.lab=0.1, yaxt='n', cex.axis=2.2, xlab='', ylab='', xlim=c(-2, 2), ylim=c(0,22)))
 with(subset(Pro_SWATH_output, padjust<0.05 & log2FC>log2(1.3)), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='red'))
 with(subset(Pro_SWATH_output, padjust<0.05 & log2FC<(-log2(1.3))), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='dodgerblue'))
@@ -310,7 +285,7 @@ abline(h=-log10(0.05), col="darkgray", lty=2)
 dev.off()
 
 
-pdf('Proliferative_DDA_Volcano_20180314.pdf', width=10, height=10)
+pdf('Proliferative_DDA_Volcano.pdf', width=10, height=10)
 with(Pro_DDA_output, plot(log2FC, -log10(padjust), pch=20, cex=2, cex.lab=0.1, yaxt='n', cex.axis=2.2, xlab='', ylab='', xlim=c(-2, 2), ylim=c(0,22)))
 with(subset(Pro_DDA_output, padjust<0.05 & log2FC>log2(1.3)), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='red'))
 with(subset(Pro_DDA_output, padjust<0.05 & log2FC<(-log2(1.3))), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='dodgerblue'))
@@ -320,10 +295,7 @@ abline(h=-log10(0.05), col="darkgray", lty=2)
 dev.off()
 
 
-
-
-
-## Step 5: Make group comparison for Stromal group and check overlap
+# Make group comparison for Stromal group and check overlap
 
 z <- c(84, 88, 24, 96, 5)
 
@@ -381,7 +353,7 @@ length(intersect(downreg$Protein, downreg_DDA$Protein))
 
 #plot
 
-pdf('Stromal_SWATH_Volcano_20180314.pdf', width=10, height=10)
+pdf('Stromal_SWATH_Volcano.pdf', width=10, height=10)
 with(Stro_SWATH_output, plot(log2FC, -log10(padjust), pch=20, cex=2, cex.lab=0.1, yaxt='n', cex.axis=2.2, xlab='', ylab='', xlim=c(-2, 2), ylim=c(0,22)))
 with(subset(Stro_SWATH_output, padjust<0.05 & log2FC>log2(1.3)), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='red'))
 with(subset(Stro_SWATH_output, padjust<0.05 & log2FC<(-log2(1.3))), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='dodgerblue'))
@@ -391,7 +363,7 @@ abline(h=-log10(0.05), col="darkgray", lty=2)
 dev.off()
 
 
-pdf('Stromal_DDA_Volcano_20180314.pdf', width=10, height=10)
+pdf('Stromal_DDA_Volcano.pdf', width=10, height=10)
 with(Stro_DDA_output, plot(log2FC, -log10(padjust), pch=20, cex=2, cex.lab=0.1, yaxt='n', cex.axis=2.2, xlab='', ylab='', xlim=c(-2, 2), ylim=c(0,22)))
 with(subset(Stro_DDA_output, padjust<0.05 & log2FC>log2(1.3)), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='red'))
 with(subset(Stro_DDA_output, padjust<0.05 & log2FC<(-log2(1.3))), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='dodgerblue'))
@@ -401,7 +373,7 @@ abline(h=-log10(0.05), col="darkgray", lty=2)
 dev.off()
 
 
-## Step 6: Make group comparison for immunoreactive group and check overlap
+# Make group comparison for immunoreactive group and check overlap
 
 z <- c(7, 61, 64:67, 75, 78:79, 82, 19, 90, 25, 38, 41, 98, 44:45, 49, 53, 55, 58:59, 4)
 
@@ -460,7 +432,7 @@ length(intersect(downreg$Protein, downreg_DDA$Protein))
 
 #plot
 
-pdf('Immunoreactive_SWATH_Volcano_20180314.pdf', width=10, height=10)
+pdf('Immunoreactive_SWATH_Volcano.pdf', width=10, height=10)
 with(Immu_SWATH_output, plot(log2FC, -log10(padjust), pch=20, cex=2, cex.lab=0.1, yaxt='n', cex.axis=2.2, xlab='', ylab='', xlim=c(-2, 2), ylim=c(0,22)))
 with(subset(Immu_SWATH_output, padjust<0.05 & log2FC>log2(1.3)), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='red'))
 with(subset(Immu_SWATH_output, padjust<0.05 & log2FC<(-log2(1.3))), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='dodgerblue'))
@@ -470,7 +442,7 @@ abline(h=-log10(0.05), col="darkgray", lty=2)
 dev.off()
 
 
-pdf('Immunoreactive_DDA_Volcano_20180314.pdf', width=10, height=10)
+pdf('Immunoreactive_DDA_Volcano.pdf', width=10, height=10)
 with(Immu_DDA_output, plot(log2FC, -log10(padjust), pch=20, cex=2, cex.lab=0.1, yaxt='n', cex.axis=2.2, xlab='', ylab='', xlim=c(-2, 2), ylim=c(0,22)))
 with(subset(Immu_DDA_output, padjust<0.05 & log2FC>log2(1.3)), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='red'))
 with(subset(Immu_DDA_output, padjust<0.05 & log2FC<(-log2(1.3))), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='dodgerblue'))
@@ -481,7 +453,7 @@ dev.off()
 
 
 
-## Step 7: Make group comparison for Differentiated group and check overlap
+# Make group comparison for Differentiated group and check overlap
 
 z <- c(1:3, 62:63, 10, 70, 73, 12:14, 16, 81, 83, 21:22, 85, 87, 89, 91, 26, 34, 36, 39, 94, 43, 46:48, 100:103, 57)
 
@@ -538,7 +510,7 @@ length(intersect(downreg$Protein, downreg_DDA$Protein))
 
 #plot
 
-pdf('Differentiated_SWATH_Volcano_20180314.pdf', width=10, height=10)
+pdf('Differentiated_SWATH_Volcano.pdf', width=10, height=10)
 with(Diff_SWATH_output, plot(log2FC, -log10(padjust), pch=20, cex=2, cex.lab=0.1, yaxt='n', cex.axis=2.2, xlab='', ylab='', xlim=c(-2, 2), ylim=c(0,22)))
 with(subset(Diff_SWATH_output, padjust<0.05 & log2FC>log2(1.3)), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='red'))
 with(subset(Diff_SWATH_output, padjust<0.05 & log2FC<(-log2(1.3))), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='dodgerblue'))
@@ -548,7 +520,7 @@ abline(h=-log10(0.05), col="darkgray", lty=2)
 dev.off()
 
 
-pdf('Differentiated_DDA_Volcano_20180314.pdf', width=10, height=10)
+pdf('Differentiated_DDA_Volcano.pdf', width=10, height=10)
 with(Diff_DDA_output, plot(log2FC, -log10(padjust), pch=20, cex=2, cex.lab=0.1, yaxt='n', cex.axis=2.2, xlab='', ylab='', xlim=c(-2, 2), ylim=c(0,22)))
 with(subset(Diff_DDA_output, padjust<0.05 & log2FC>log2(1.3)), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='red'))
 with(subset(Diff_DDA_output, padjust<0.05 & log2FC<(-log2(1.3))), points(log2FC, -log10(padjust), pch=20, cex=2.2, col='dodgerblue'))
@@ -556,11 +528,3 @@ axis(2, las=2, cex.axis=2.2)
 abline(v=c(log2(1.3),-log2(1.3)), col=c("darkgray","darkgray"), lty=c(2,2))
 abline(h=-log10(0.05), col="darkgray", lty=2)
 dev.off()
-
-
-
-
-
-
-
-
